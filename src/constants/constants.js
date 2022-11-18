7de284c88c1f36897f2aefefb88c92bb06cd374e
Yaseen Nur <paradushex@gmail.com>
2022-11-18 09:02:11 +0600

    moved constants to the config file

---
+++ src/constants/constants.js
@@ -0,0 +1,26 @@
+export const heroSection = {
+  description:
+    "Reinforz is an innovative platform for learners, students, and educators to collaborate, reinforce and share their learning with the help of artificial intelligence.",
+};
+
+export const featureSection = {
+  questionGenerationTitle:
+    "Generate questions using our state of the art NLP models",
+  questionGenerationDescription:
+    "Just provide a passage and our NLP model will generate questions for you. You can currently only generate true-false questions but we will be supporting other question types very soon!",
+  quizExploreTitle: "Explore quizzes created by others or create your own",
+  quizExploreDescription:
+    "Discover a variety of quizzes created by other users on diverse topics. You can also create your own quizzes on any topic. Make it public if you want others to play your quiz or make it private for own practice.",
+  quizAssessmentTitle: "Take the quiz and assess your mastery",
+  quizAssessmentDescription:
+    " We have a beautiful and detailed user interface for taking any quiz. You will see the weight, difficulty and time allocated for each question in a quiz.",
+  reportTitle: "Receive a detailed report on your performance",
+  reportDescription:
+    "After each quiz we provide a detailed performance report. Locate where you are making the most mistakes and improve upon it.",
+  pastReportTitle: "Track your progress through previous reports",
+  pastReportDescription:
+    "We store all your previous quiz performance report. So that you can look back and see how you have progressed.",
+  leaderboardTitle: "Compete for top spots in the quiz leaderboards!",
+  leaderboardDescription:
+    "You are added points for playing the quizzes and many other activities! Compare yourself with other users in the Global and local quiz leaderboards!",
+};
