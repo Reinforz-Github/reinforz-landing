35d771827972b99768b07ab9273ac914704545bb
arionrefat <arionrefat@gmail.com>
2022-11-22 17:17:38 +0600

    added appfooter and blog react components

---
+++ src/ReactCompoments/AppFooterReact.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { footerLinks, footerSocialLinks } from "../constants/constants";
+
+export const AppFooterReact = () => {
+  return (
+    <footer class="py-20 bg-gray-3">
+      <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+        <div class="flex flex-col items-center">
+          <div class="m-auto w-8/12 md:w-10/12 lg:w-8/12 xl:w-6/12 flex md:flex-col justify-between md:justify-center items-center">
+            <div class="flex items-center justify-center text-white sm:w-7/12 md:justify-center">
+              <ul
+                role="list"
+                class="md:space-x-8 md:space-y-0 space-y-5 list md:flex"
+              >
+                {footerLinks.map((footer) => (
+                  <li>
+                    <a href={footer.link} class="transition hover:text-primary">
+                      {footer.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div class="hidden invisible md:visible min-w-full py-2 border-b-2 border-gray-800 sm:flex justify"></div>
+            <div class="flex mt-4 items-center justify-center text-white">
+              <ul
+                role="list"
+                class="md:space-x-8 list md:space-y-0 space-y-5 md:flex"
+              >
+                {footerSocialLinks.map((social) => (
+                  <li>
+                    <a
+                      href={social.link}
+                      target="_blank"
+                      class="flex items-center space-x-3 transition hover:text-primary"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        class="w-5"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d={social.svgPath}></path>
+                      </svg>
+                      <span>{social.title}</span>
+                    </a>
+                  </li>
+                ))}
+                <li>
+                  <a
+                    href="https://www.linkedin.com/company/reinforz/"
+                    target="_blank"
+                    class="flex items-center space-x-3 transition hover:text-primary"
+                  >
+                    <img src="./linked.svg" width="26" />
+                    <span>LinkedIn</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="text-gray-500 pt-20">
+            ©️ 2022 Reinforz All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- src/ReactCompoments/FeaturesReact.jsx
+++ src/ReactCompoments/FeaturesReact.jsx
@@ -5,27 +5,33 @@ import React from "react";
 const FeaturesReact = () => {
   return (
     <div>
-      <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
-        {featureSection.map((feature) => {
-          <div className="mb-21 lg:mb-40">
-            <div className="space-y-7 justify-between text-gray-600 md:flex flex-row-reverse md:gap-6 md:space-y-0 lg:gap-12 lg:items-center">
-              <div className="md:4/12 lg:w-2/3">
-                <img
-                  src={feature.image}
-                  alt="image"
-                  width=""
-                  height=""
-                  className="w-full"
-                />
-              </div>
-              <div className="md:6/12 lg:w-1/2">
-                <h1 className="text-3xl text-center sm:text-left font-bold md:text-4xl text-primary">
-                  {feature.title}
-                </h1>
-                <p className="my-9 text-gray-300">{feature.description}</p>
+      <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6 mt-48">
+        {featureSection.map((feature, index) => {
+          return (
+            <div class="mb-21 lg:mb-40">
+              <div
+                class={`space-y-7 justify-between text-gray-600 md:flex ${
+                  index % 2 == 0 ? "flex-row-reverse" : ""
+                } md:gap-6 md:space-y-0 lg:gap-12 lg:items-center`}
+              >
+                <div class="md:4/12 lg:w-2/3">
+                  <img
+                    src={feature.image}
+                    alt="image"
+                    width=""
+                    height=""
+                    class="w-full"
+                  />
+                </div>
+                <div class="md:6/12 lg:w-1/2">
+                  <h1 class="text-3xl text-center sm:text-left font-bold md:text-4xl text-primary">
+                    {feature.title}
+                  </h1>
+                  <p class="my-9 text-gray-300">{feature.description}</p>
+                </div>
               </div>
             </div>
-          </div>;
+          );
         })}
       </div>
     </div>
