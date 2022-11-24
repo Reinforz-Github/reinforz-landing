9d68280806d43b570a505ac862456efb223e6bd1
arionrefat <arionrefat@gmail.com>
2022-11-24 19:14:58 +0600

    updated privacy and toc page

--- src/ReactCompoments/AppFooterReact.jsx
+++ src/ReactCompoments/AppFooterReact.jsx
@@ -53,6 +53,17 @@ export const AppFooterReact = () => {
               </ul>
             </div>
           </div>
+          <div class='flex text-white pt-10'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              class='w-5 mr-2'
+              viewBox='0 0 25 25'
+              fill='currentColor'
+            >
+              <path d='M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z' />
+            </svg>
+            admin@reinforz.xyz
+          </div>
           <div class='text-gray-500 pt-20'>
             ©️ 2022 Reinforz All rights reserved.
           </div>
