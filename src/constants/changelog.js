934fc8fc858aaa6b407b7d5efc457c610273bf00
arionrefat <arionrefat@gmail.com>
2022-11-25 23:11:35 +0600

    added sprint23

--- src/constants/changelog.js
+++ src/constants/changelog.js
@@ -1,12 +1,19 @@
 export const changeLog = [
   {
-    sprint: 22,
-    startDate: "02-12-2022",
-    features: ["I have added this feature", "we should add this feature that"],
-    improvements: [
-      "We have improved this feature ",
-      " We will be doing in the UI improvements",
+    sprint: 23,
+    startDate: "19-11-2022",
+    features: [
+      `You can now add explanations for the answers to your fib questions. Click on "Add explanation" ➕ to add an explanation for each answer or "Remove explanation" to remove it.`,
+      `You will now be able to view the hints you've used per question on the report page. Click on the "Hints used" button, and it will open up a modal with all the hints, along with an 👁️ icon to indicate that you've used that hint, Click on "Generate Questions," paste your passage, and let our model generate questions.`,
+      `You can now view the explanations added for each answer of snippet and fib questions 👀`,
+      `We have added support for superscript and subscript formatting to the question editor. Click on the X^2 or X_2 buttons on top to enable them`,
+      `Aside from the main question, you can now add alternate questions. Click on Add alternate to type the alternate with rte support. When playing the question, any of those questions will be used. This will ensure that nobody memorizes the question while answering 🧠 `,
+      `We now show the explanation for each answer of your fib and snippet questions on the report page (if they exist). This will help you understand the reason behind the answer 🧠`,
     ],
-    fixes: ["We have fixed this ", "We are eager to fixed that"],
+    improvements: [
+      `We have relocated our privacy and terms page to a static route under our landing page.
+ And we also made it responsive! 🚀`,
+    ],
+    fixes: [`None`],
   },
 ];
